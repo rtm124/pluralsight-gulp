40ded0a18624ede5ad5b054532f40771848c69d4
rtm124 <rtm124@gmail.com>
2018-03-31 14:51:45 -0700

    GULP - CSS Compilation

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,19 @@
 module.exports = function() {
+    var client = './src/client/';
     var config = {
+        temp: './.tmp/',
 
+        /**
+         * File Paths
+         * 
+         */
         //all the js i want to vet
         alljs: [
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+
+        less: client + 'styles/styles.less'
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var config = require('./gulp.config.js')();
+var del = require('del');
 var $ = require('gulp-load-plugins')({lazy: true});
 
 //var jshint = require('gulp-jshint');
@@ -17,7 +18,35 @@ gulp.task('vet', function() {
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish',{verbose: true}))
     .pipe($.jshint.reporter('fail'));
-}); 
+});
+
+gulp.task('styles', function() {
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+        .pipe(gulp.dest(config.temp));
+});
+
+gulp.task('clean-styles',  function(done) {
+    var files = config.temp + '**/*.css';
+    clean(files, done);
+});
+
+gulp.task('less-watcher', function() {
+    gulp.watch([config.less], ['styles']);
+});
+
+//////////
+
+function clean(path, done) {
+    log('Cleaning: '+ $.util.colors.blue(path));
+    del(path, done);
+
+}
 
 function log(msg) {
    if (typeof(msg) === 'object') {
